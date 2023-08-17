a43f29205346195d018f051136d58bc499065104
Hashem <43625073+lesqueda@users.noreply.github.com>
2023-08-16 21:28:12 -0500

    Código de Explicación Interfaces Gráficas

---
+++ com/mycompany/mavenproject1/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import GUI.interfazPrincipal;
+
+/**
+ *
+ * @author leonardo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        interfazPrincipal gui = new interfazPrincipal();
+        
+        gui.setVisible(true);
+        gui.setLocationRelativeTo(null);
+    }
+}
